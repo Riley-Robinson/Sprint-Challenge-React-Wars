a2765e5db47bac71058ddf5c7e626b9ce95c2031
Riley-Robinson <rrtoinfinity@gmail.com>
2020-03-20 12:02:36 -0400

    "curetly done with app.js going to poepl card then back to app.js"

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+//import PeopleCard from "./componenets/PeopleCard";
 
-const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+export default function App()  {
+  const [people, setPeople] = useState([]);
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-
+      useEffect(() => {
+        axios
+          .get("https://swapi.co/api/people/")
+          .then(response => {
+            console.log(response.data.result);
+           
+            setPeople(response.data.result);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }, []);
+  
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
@@ -16,4 +26,10 @@ const App = () => {
   );
 }
 
-export default App;
+// Try to think through what state you'll need for this app before starting. Then build out
+  // the state properties here.
+
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // side effect in a component, you want to think about which state and/or props it should
+  // sync up with, if any.
+
