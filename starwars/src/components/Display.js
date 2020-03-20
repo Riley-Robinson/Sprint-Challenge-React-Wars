7083d18dc934a9dd8ecea0b558ded35afc4172af
Riley-Robinson <rrtoinfinity@gmail.com>
2020-03-20 12:56:46 -0400

    "style done for display"

--- starwars/src/components/Display.js
+++ starwars/src/components/Display.js
@@ -4,8 +4,15 @@ import Axios from 'axios';
 import styled from 'styled-components';
 
 
-
-
+const SpaceBox = styled.div`
+	height: 80vh;
+	margin: 25px auto;
+	overflow: auto;
+	background-color: rgba(0,0,0,0.50);
+    align-items:center;
+    justify-content:space-between;
+    padding-bottom:20px;
+`;
 
 const Display = () => {
 
@@ -21,11 +28,11 @@ const Display = () => {
         }, [])
 
 	return (
-		<div>
+		<SpaceBox>
 			{characters.map((data, i) => (
 	            <PeopleCard data={data} key={i}/>
 	        ))}
-	    </div>
+	    </SpaceBox>
 	)
 
 }
